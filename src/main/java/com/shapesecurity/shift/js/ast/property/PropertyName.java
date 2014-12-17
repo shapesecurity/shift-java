6fe9f10f781ac51e2784793a6f6e49023a65675c
Bei Zhang <bzhang@shapesecurity.com>
2014-12-17 14:55:10 -0800

    ObjectPropertyName -> PropertyName

--- src/main/java/com/shapesecurity/shift/js/ast/property/PropertyName.java
+++ src/main/java/com/shapesecurity/shift/js/ast/property/PropertyName.java
@@ -33,7 +33,7 @@ import javax.annotation.Nonnull;
 public final class PropertyName extends Node {
   @Nonnull
   public final String value;
-  public final ObjectPropertyNameKind kind;
+  public final PropertyNameKind kind;
 
   public PropertyName(@Nonnull PropertyName node) {
     super();
@@ -44,7 +44,7 @@ public final class PropertyName extends Node {
   public PropertyName(@Nonnull Identifier ident) {
     super();
     this.value = ident.name;
-    this.kind = ObjectPropertyNameKind.Identifier;
+    this.kind = PropertyNameKind.Identifier;
   }
 
   public PropertyName(@Nonnull LiteralStringExpression str) {
@@ -58,13 +58,13 @@ public final class PropertyName extends Node {
   public PropertyName(@Nonnull String str) {
     super();
     this.value = str;
-    this.kind = ObjectPropertyNameKind.String;
+    this.kind = PropertyNameKind.String;
   }
 
   public PropertyName(double d) {
     super();
     this.value = D2A.d2a(d);
-    this.kind = ObjectPropertyNameKind.Number;
+    this.kind = PropertyNameKind.Number;
   }
 
   @Nonnull
@@ -101,14 +101,14 @@ public final class PropertyName extends Node {
         this.value.equals(((PropertyName) object).value);
   }
 
-  public static enum ObjectPropertyNameKind {
+  public static enum PropertyNameKind {
     Identifier("identifier"),
     String("string"),
     Number("number");
     @Nonnull
     public final String name;
 
-    private ObjectPropertyNameKind(@Nonnull String name) {
+    private PropertyNameKind(@Nonnull String name) {
       this.name = name;
     }
   }
