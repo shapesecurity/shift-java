4f5b96f2e58e1d5851a9d9a927a0d79cd9496eac
Linda Zheng <lzheng@shapesecurity.com>
2015-06-09 14:15:34 -0700

    passed all while statement tests. support for binaryExpressions

--- src/test/java/com/shapesecurity/shift/parser/statements/WhileStatementTest.java
+++ src/test/java/com/shapesecurity/shift/parser/statements/WhileStatementTest.java
@@ -2,6 +2,8 @@ package com.shapesecurity.shift.parser.statements;
 
 import com.shapesecurity.functional.data.ImmutableList;
 import com.shapesecurity.shift.ast.*;
+import com.shapesecurity.shift.ast.operators.BinaryOperator;
+import com.shapesecurity.shift.ast.operators.UpdateOperator;
 import com.shapesecurity.shift.parser.Assertions;
 import com.shapesecurity.shift.parser.JsError;
 import org.junit.Test;
@@ -17,5 +19,12 @@ public class WhileStatementTest extends Assertions {
       new LiteralBooleanExpression(true),
       new ExpressionStatement(new CallExpression(new IdentifierExpression("doSomething"), ImmutableList.nil())
     )));
+
+    testScript("while (x < 10) {x++; y--; }", new WhileStatement(new BinaryExpression(BinaryOperator.LessThan,
+        new IdentifierExpression("x"), new LiteralNumericExpression(10.0)), new BlockStatement(new Block(
+        ImmutableList.list(new ExpressionStatement(new UpdateExpression(false, UpdateOperator.Increment,
+            new BindingIdentifier("x"))), new ExpressionStatement(new UpdateExpression(false, UpdateOperator.Decrement,
+            new BindingIdentifier("y"))))
+    ))));
   }
 }
