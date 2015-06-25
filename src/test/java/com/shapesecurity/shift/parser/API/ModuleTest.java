c51925e5715fdcebe9982f2918aa300fd40e8119
Linda Zheng <lzheng@shapesecurity.com>
2015-06-25 14:12:05 -0700

    added module and script tests for the API directory

--- src/test/java/com/shapesecurity/shift/parser/API/ModuleTest.java
+++ src/test/java/com/shapesecurity/shift/parser/API/ModuleTest.java
@@ -1,5 +1,7 @@
 package com.shapesecurity.shift.parser.API;
 
+import com.shapesecurity.functional.data.ImmutableList;
+import com.shapesecurity.shift.ast.Module;
 import com.shapesecurity.shift.parser.Assertions;
 import com.shapesecurity.shift.parser.JsError;
 import org.junit.Test;
@@ -10,6 +12,7 @@ import org.junit.Test;
 public class ModuleTest extends Assertions {
   @Test
   public void testModule() throws JsError {
-
+    testModule("", new Module(ImmutableList.nil(), ImmutableList.nil()));
+//    testModuleFailure("/*", 0, "Unexpected end of input");
   }
 }
