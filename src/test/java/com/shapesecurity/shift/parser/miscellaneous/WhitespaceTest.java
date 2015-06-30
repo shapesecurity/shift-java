ac095a86a05998166b626a59ccf399645456dd8f
Linda Zheng <lzheng@shapesecurity.com>
2015-06-30 12:48:12 -0700

    added tests for arraybinding. fixed some more tests

--- src/test/java/com/shapesecurity/shift/parser/miscellaneous/WhitespaceTest.java
+++ src/test/java/com/shapesecurity/shift/parser/miscellaneous/WhitespaceTest.java
@@ -8,9 +8,6 @@ import com.shapesecurity.shift.parser.Assertions;
 import com.shapesecurity.shift.parser.JsError;
 import org.junit.Test;
 
-/**
- * Created by u478 on 6/16/15.
- */
 public class WhitespaceTest extends Assertions {
   @Test
   public void testWhitespace() throws JsError {
