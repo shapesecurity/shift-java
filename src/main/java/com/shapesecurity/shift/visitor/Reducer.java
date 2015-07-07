4a240970e8f1030f0a28ee68e18fcbec85e8f51d
Linda Zheng <lzheng@shapesecurity.com>
2015-07-07 10:53:08 -0700

    commiting stuff from yesterday: added implementations for all reduce... methods.need to fix parameterization of ReducerP and Director method signatures

--- src/main/java/com/shapesecurity/shift/visitor/Reducer.java
+++ src/main/java/com/shapesecurity/shift/visitor/Reducer.java
@@ -25,6 +25,6 @@ public interface Reducer<State>
   State, State, State, State, State, State, State, State, State, State, State, State, State, State,
   State, State, State, State, State, State, State, State, State, State, State, State, State, State,
   State, State, State, State, State, State, State, State, State, State, State, State, State, State,
-  State, State, State, State, State, State, State, State, State, State
+  State, State, State, State, State, State, State, State, State
   > {
 }
