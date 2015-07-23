11857e0d5c69641069c4a1205de39060f1ba8739
Linda Zheng <lzheng@shapesecurity.com>
2015-07-23 14:52:18 -0700

    added es6 tests for codegen

--- src/test/java/com/shapesecurity/shift/codegen/CodeGenTest.java
+++ src/test/java/com/shapesecurity/shift/codegen/CodeGenTest.java
@@ -22,6 +22,7 @@ import com.shapesecurity.functional.data.Maybe;
 import com.shapesecurity.shift.ast.*;
 import com.shapesecurity.shift.parser.JsError;
 import com.shapesecurity.shift.parser.Parser;
+import jdk.nashorn.internal.ir.annotations.Immutable;
 import junit.framework.TestCase;
 import org.jetbrains.annotations.NotNull;
 import org.junit.Test;
@@ -81,8 +82,8 @@ public class CodeGenTest extends TestCase {
   @Test
   public void testCodeGenDirectives() throws JsError {
     test("\"use strict\"");
-    test("\"use\\u0020strict\"");
-    testShift("\"use\\u0020strict\"", new Script(ImmutableList.<Directive>list(new Directive("use strict")), ImmutableList.<Statement>nil()));
+    test("\"use\u0020strict\"");
+    testShift("\"use\u0020strict\"", new Script(ImmutableList.list(new Directive("use strict")), ImmutableList.nil()));
   }
 
   @Test
@@ -98,6 +99,16 @@ public class CodeGenTest extends TestCase {
   }
 
   @Test
+  public void testSpreadElement() throws JsError {
+    test("[...a]");
+    test("[...a,...b]");
+    test("[...a,b,...c]");
+    test("[...a=b]");
+    test("[...(a,b)]");
+    test("f(...a)");
+  }
+
+  @Test
   public void testObjectExpression() throws JsError {
     test("({})");
     test("({a:1})", "({a:1,})");
@@ -109,6 +120,76 @@ public class CodeGenTest extends TestCase {
     test("({get a(){;},set a(param){;},b:1})");
     test("({a:(a,b)})");
     test("({2e308:0})", "({2e308:0})");
+
+    // from js
+    test("({})");
+    test("({a:1})", "({a:1,})");
+    test("({}.a--)");
+    test("({1:1})", "({1.0:1})");
+    test("({a:b})");
+    test("({255:0})", "({0xFF:0})");
+    test("({63:0})", "({0o77:0})");
+    test("({3:0})", "({0b11:0})");
+    test("({0:0})", "({0.:0})");
+    test("({0:0})", "({.0:0})");
+    test("({.1:0})", "({0.1:0})");
+    test("({[a]:b})");
+    test("({a:b})", "({\"a\":b})");
+    test("({\" \":b})");
+    test("({get a(){;}})");
+    test("({set a(param){;}})");
+    test("({get a(){;},set a(param){;},b:1})");
+    test("({a:(a,b)})");
+    test("({a})");
+  }
+
+  @Test
+  public void testArrayBinding() throws JsError {
+    test("[]=0");
+    test("[...a]=0");
+    test("[a,...a]=0");
+    test("[a,a=0,...a]=0");
+    test("[,,]=0");
+    test("[,...a]=0");
+  }
+
+  @Test
+  public void testBindingPropertyIdentifier() throws JsError {
+    test("({a=0}=0)");
+  }
+
+  @Test
+  public void testBindingPropertyProperty() throws JsError {
+    test("({a:b}=0)");
+  }
+
+  @Test
+  public void testBindingWithDefault() throws JsError {
+    test("[a=0]=0");
+    test("({a:b=0}=0)");
+  }
+
+  @Test
+  public void testClassDeclaration() throws JsError {
+    test("class A{}");
+    test("class A extends B{}");
+  }
+
+  @Test
+  public void testClassExpression() throws JsError {
+    test("(class{})");
+    test("(class A{})");
+    test("(class A extends B{})");
+    test("(class extends B{})");
+  }
+
+  @Test
+  public void testClassElement() throws JsError {
+    test("(class{a(){}})");
+    test("(class{*a(){}})");
+    test("(class{static a(){}})");
+    test("(class{static*a(){}})");
+    test("(class{constructor(){}})");
   }
 
   @Test
@@ -119,6 +200,10 @@ public class CodeGenTest extends TestCase {
   @Test
   public void testAssignment() throws JsError {
     test("a=b");
+  }
+
+  @Test
+  public void testCompoundAssignment() throws JsError {
     test("a+=b");
     test("a*=b");
     test("a%=b");
@@ -128,9 +213,14 @@ public class CodeGenTest extends TestCase {
     test("a/=b");
     test("a|=b");
     test("a^=b");
-    test("a,b^=b");
-    test("b^=b,b");
-    test("b^=(b,b)");
+    test("a,b=c");
+    test("a=b,c");
+    test("a=(b,c)");
+    test("a,b^=c");
+    test("a^=b,c");
+    test("a^=(b,c)");
+    test("a.b=0");
+    test("a[b]=0");
   }
 
   @Test
@@ -157,7 +247,7 @@ public class CodeGenTest extends TestCase {
 
   @Test
   public void testLogicalAnd() throws JsError {
-    test("a||b");
+    test("a&&b");
   }
 
   @Test
@@ -223,6 +313,8 @@ public class CodeGenTest extends TestCase {
     test("a+(b+b)");
     test("a+(b<<b)");
     test("a+b<<b");
+    test("(a<<b)+(c>>d)");
+    test("a*b+c/d", "(a*b)+(c/d)");
   }
 
   @Test
@@ -234,6 +326,8 @@ public class CodeGenTest extends TestCase {
     test("a%(b%c)");
     test("a+b%c");
     test("(a+b)%c");
+    test("!a*b");
+    test("a*(b+c)");
   }
 
   @Test
@@ -267,6 +361,7 @@ public class CodeGenTest extends TestCase {
     test("new a");
     test("new a(a)");
     test("new a(a,b)");
+    test("new this.a");
     test("a()");
     test("a(a)");
     test("a(a,b)");
@@ -282,6 +377,288 @@ public class CodeGenTest extends TestCase {
     test("new((new a)().a)", "new((new a)()).a");
     test("new a.a");
     test("new(a().a)");
+  }
+
+  @Test
+  public void testLiteralRegExpExpression() throws JsError {
+    test("/a/");
+    test("/a/i");
+    test("/a/ig");
+    test("/a\\s/ig");
+    test("/a\\r/ig");
+    test("/a\\r/ instanceof 3");
+    test("/a\\r/g instanceof 3");
+  }
+
+  @Test
+  public void testLiteralBooleanExpression() throws JsError {
+    test("true");
+    test("false");
+  }
+
+  @Test
+  public void testLiteralNullExpression() throws JsError {
+    test("null");
+    test("null", "nul\\u006c");
+  }
+
+  @Test
+  public void testFunctionDeclaration() throws JsError {
+    test("function f(){}");
+    test("function*f(){}");
+    test("function f(a){}");
+    test("function f(a,b){}");
+    test("function f(a,b,...rest){}");
+  }
+
+  @Test
+  public void testFunctionExpression() throws JsError {
+    test("(function(){})");
+    test("(function f(){})");
+    test("(function*(){})");
+    test("(function*f(){})");
+  }
+
+  @Test
+  public void testArrowExpression() throws JsError {
+    test("a=>a");
+    test("()=>a");
+    test("a=>a", "(a)=>a");
+    test("(...a)=>a");
+    test("(a,...b)=>a");
+    test("(a=0)=>a");
+    test("(a,b)=>a");
+    test("({a})=>a");
+    test("({a=0})=>a");
+    test("([a])=>a");
+    test("a=>({})");
+    test("a=>{}");
+    test("a=>{({})}");
+    test("a=>{0;return}");
+    test("()=>function(){}");
+    test("()=>class{}");
+  }
+
+  @Test
+  public void testNewTargetExpression() throws JsError {
+    test("function f(){new.target}");
+    test("function f(){new.target}", "function f() { new . target }");
+  }
+
+  @Test
+  public void testTemplateExpression() throws JsError {
+      test("``");
+      test("````");
+      test("a``");
+      test("a.b``");
+      test("a[b]``");
+      test("a()``");
+      test("(a+b)``");
+      test("(function(){}``)", "(function(){})``");
+      test("(class{}``)", "(class{})``");
+      test("({}``)", "({})``");
+      test("`a`");
+      test("a`a`");
+      test("`a${b}c`");
+      test("`${a}`");
+      test("`${a}${b}`");
+      test("` ${a} ${b} `");
+      test("` ${a} ${b} `", "` ${ a } ${ b } `");
+      test("`a\\${b}c`");
+      test("``.a");
+      test("``()");
+      test("new``");
+      test("new``", "new ``()");
+      test("new``(a)", "new ``(a)");
+  }
+
+  @Test
+  public void testSuper() throws JsError {
+    test("class A extends B{constructor(){super()}}");
+    test("({m(){super.m()}})");
+  }
+
+  @Test
+  public void testYieldExpression() throws JsError {
+    test("function*f(){yield}");
+    test("function*f(){yield a}");
+    test("function*f(){yield 0}");
+    test("function*f(){yield{}}");
+    test("function*f(){yield a+b}");
+    test("function*f(){yield a=b}");
+    test("function*f(){yield(a,b)}");
+    test("function*f(){f(yield,yield)}");
+    test("function*f(){f(yield a,yield b)}");
+    test("function*f(){yield yield yield}");
+  }
+
+  @Test
+  public void testYieldGeneratorExpression() throws JsError {
+    test("function*f(){yield*a}");
+    test("function*f(){yield*0}");
+    test("function*f(){yield*{}}");
+    test("function*f(){yield*a+b}");
+    test("function*f(){yield*a=b}");
+    test("function*f(){yield*(a,b)}");
+    test("function*f(){f(yield*a,yield*b)}");
+    test("function*f(){yield*yield*(yield)*(yield)}");
+  }
+
+  @Test
+  public void testForOfStatement() throws JsError {
+    test("for(a of b);");
+    test("for([a]of[b]);");
+    test("for(let[a]of[b]);");
+  }
+
+  @Test
+  public void testLiteralNumericExpression() throws JsError {
+    test("0");
+    test("0", "0x0");
+    test("0", "0o0");
+    test("0", "0b0");
+    test("1");
+    test("2");
+    test("0x38D7EA4C68001");
+    test("15e5", "1500000");
+    test("155e3", "155000");
+    test(".1");
+    test(".1", "0.1");
+    // floats
+    test("1.1.valueOf()");
+    test("15..valueOf()");
+    test("1..valueOf()");
+    test("1e300.valueOf()", "1e+300.valueOf()");
+    test("8e15.valueOf()", "8000000000000000..valueOf()");
+    test("1e20", "100000000000000000001");
+    test("10..valueOf()", "1e1.valueOf()");
+    test("100", "1e2");
+    test("1.3754889325393114", "1.3754889325393114");
+    test("1.3754889325393114e24", "0x0123456789abcdefABCDEF");
+    test("4.185580496821357e298", "4.1855804968213567e+298");
+    test("5.562684646268003e-308", "5.5626846462680035e-308");
+    test("5.562684646268003e-309", "5.5626846462680035e-309");
+    test("2147483648", "2147483648.0");
+    test("1e-7");
+    test("1e-8");
+    test("1e-9");
+    test("1e308", "1e+308");
+    test("1e308");
+    test("1e-308");
+  }
+
+  @Test
+  public void testLiteralInfinityExpression() throws JsError {
+    test("2e308", "2e308");
+    test("1+2e308", "1+2e308");
+    test("2e308", "3e308");
+  }
+
+  @Test
+  public void testLiteralStringExpression() throws JsError {
+    test("(\"\")");
+    test("(\"\")", "('')");
+    test("(\"a\")", "('a')");
+    test("('\"')", "(\"\\\"\")");
+    test("(\"a\")", "('a')");
+    test("(\"'\")", "('\\'')");
+    test("(\"a\")");
+    test("('\"')");
+    test("(\"\\b\\n\\r\\t\\v\\f\\\\\\\"'\\u2028\\u2029日本\")");
+  }
+
+  @Test
+  public void testImport() throws JsError {
+    test("import\"m\"");
+    test("import\"m\";0");
+    test("import a from\"m\"");
+    test("import{a}from\"m\"");
+    test("import{a,b}from\"m\"");
+    test("import a,{b}from\"m\"");
+    test("import a,{b,c}from\"m\"");
+    test("import a,{b,c}from\"m\";0");
+    test("import\"m\"", "import {} from \"m\"");
+    test("import a from\"m\"", "import a,{}from \"m\"");
+  }
+
+  @Test
+  public void testImportNamespace() throws JsError {
+    test("import*as a from\"m\"");
+    test("import*as a from\"m\";0");
+    test("import a,*as b from\"m\"");
+  }
+
+  @Test
+  public void testImportSpecifier() throws JsError {
+    test("import{a}from\"m\"");
+    test("import{a}from\"m\";0");
+    test("import{a as b}from\"m\"");
+    test("import{a,b}from\"m\"");
+    test("import{a,b as c}from\"m\"");
+    test("import{a as b,c}from\"m\"");
+    test("import{a as b,c as d}from\"m\"");
+  }
+
+  @Test
+  public void testExportAllFrom() throws JsError {
+    test("export*from\"m\"");
+    test("export*from\"m\";0");
+  }
+
+  @Test
+  public void testExportFrom() throws JsError {
+    test("export{}from\"m\"");
+    test("export{}from\"m\";0");
+    test("let a;export{a}from\"m\"");
+    test("let a,b;export{a,b}from\"m\"");
+    test("export{}");
+    test("let a;export{a}");
+    test("let a,b;export{a,b}");
+  }
+
+  @Test
+  public void testExport() throws JsError {
+    test("export var a");
+    test("export var a;0");
+    test("export var a=0");
+    test("export var a,b");
+    test("export var a=0,b=0");
+    test("export const a=0");
+    test("export let a");
+    test("export function f(){}");
+    test("export function f(){}0");
+    test("export class A{}");
+    test("export class A{}0");
+  }
+
+  @Test
+  public void testExportDefault() throws JsError {
+    test("export default function(){}");
+    test("export default function(){}0");
+    test("export default 0");
+    test("export default 0;0");
+    test("export default function f(){}");
+    test("export default function*f(){}");
+    test("export default class A{}");
+    test("export default(class{})");
+    test("export default(function(){})");
+    test("export default{}");
+  }
+
+  @Test
+  public void testExportSpecifier() throws JsError {
+    test("let a;export{a}");
+    test("let a,b;export{a as b}");
+    test("let a,b;export{a,b}");
+    test("let a,b,c;export{a,b as c}");
+    test("let a,b,c;export{a as b,c}");
+    test("let a,b,c,d;export{a as b,c as d}");
+  }
+
+  @Test
+  public void testModule() throws JsError {
+    test("");
+    test("a;a");
   }
 
   @Test
@@ -404,6 +781,11 @@ public class CodeGenTest extends TestCase {
     test("for(var a=(3 in 5)in 1);");
     test("for(var a=(3 in 5==7 in 4)in 1);");
     test("for(var a=1+1 in 1);");
+
+    //from js
+    test("for(var a in 1);");
+    testScript("for((let)in 1);");
+    test("for(a in 1);");
   }
 
   @Test
@@ -505,6 +887,7 @@ public class CodeGenTest extends TestCase {
   public void testWithStatement() throws JsError {
     test("with(0);");
     test("with(0)with(0);");
+    testShift("with(null", new Script(ImmutableList.nil(), ImmutableList.list(new WithStatement(new LiteralNullExpression(), new EmptyStatement()))));
   }
 
   @Test
