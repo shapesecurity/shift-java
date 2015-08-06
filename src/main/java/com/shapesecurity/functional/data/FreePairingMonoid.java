45e12785539116754f40c2184b048bebf198e840
Kevin Gibbons <bakkot@gmail.com>
2015-08-05 18:02:27 -0700

    multireducer

---
+++ src/main/java/com/shapesecurity/functional/data/FreePairingMonoid.java
@@ -0,0 +1,28 @@
+package com.shapesecurity.functional.data;
+
+import com.shapesecurity.functional.Pair;
+import org.jetbrains.annotations.NotNull;
+
+public final class FreePairingMonoid<A, B> implements Monoid<Pair<A, B>> {
+    @NotNull
+    private final Monoid<A> monoidA;
+    @NotNull
+    private final Monoid<B> monoidB;
+
+    public FreePairingMonoid(@NotNull Monoid<A> monoidA, @NotNull Monoid<B> monoidB) {
+        this.monoidA = monoidA;
+        this.monoidB = monoidB;
+    }
+
+    @NotNull
+    @Override
+    public Pair<A, B> identity() {
+        return new Pair<>(this.monoidA.identity(), this.monoidB.identity());
+    }
+
+    @NotNull
+    @Override
+    public Pair<A, B> append(Pair<A, B> a, Pair<A, B> b) {
+        return new Pair<>(monoidA.append(a.a, b.a), monoidB.append(a.b, b.b));
+    }
+}
