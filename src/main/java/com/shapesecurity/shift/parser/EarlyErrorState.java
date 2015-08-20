c406188d589cd4ccd6065c33824f87966ab00853
Kevin Gibbons <bakkot@gmail.com>
2015-08-20 10:08:08 -0700

    move MultiHashTable to functional

--- src/main/java/com/shapesecurity/shift/parser/EarlyErrorState.java
+++ src/main/java/com/shapesecurity/shift/parser/EarlyErrorState.java
@@ -6,6 +6,7 @@ import com.shapesecurity.functional.Pair;
 import com.shapesecurity.functional.data.HashTable;
 import com.shapesecurity.functional.data.ImmutableList;
 import com.shapesecurity.functional.data.Monoid;
+import com.shapesecurity.functional.data.MultiHashTable;
 import com.shapesecurity.shift.ast.BindingIdentifier;
 import com.shapesecurity.shift.ast.BreakStatement;
 import com.shapesecurity.shift.ast.ContinueStatement;
@@ -1260,84 +1261,6 @@ public class EarlyErrorState {
         );
     }
 
-
-    // This class does not distinguish between "key is present, but associated with empty list" and "key is not present". If you need that, don't use this class.
-    public static class MultiHashTable<K, V> { // TODO should be elsewhere... and better
-        @NotNull
-        private final HashTable<K, ImmutableList<V>> data;
-
-        private MultiHashTable(@NotNull HashTable<K, ImmutableList<V>> data) {
-            this.data = data;
-        }
-
-        @NotNull
-        public static <K, V> MultiHashTable<K, V> empty() {
-            return new MultiHashTable<>(HashTable.empty());
-        }
-
-        @NotNull
-        public static <K, V> MultiHashTable<K, V> emptyP() {
-            return new MultiHashTable<>(HashTable.emptyP());
-        }
-
-        @NotNull
-        public MultiHashTable<K, V> put(@NotNull K key, @NotNull V value) {
-            return new MultiHashTable<>(this.data.put(key, ImmutableList.cons(value, this.data.get(key).orJust(ImmutableList.nil()))));
-        }
-
-        @NotNull
-        public MultiHashTable<K, V> remove(@NotNull K key) {
-            return new MultiHashTable<>(this.data.remove(key));
-        }
-
-        @NotNull
-        public ImmutableList<V> get(@NotNull K key) {
-            return this.data.get(key).orJust(ImmutableList.nil());
-        }
-
-        @NotNull
-        public MultiHashTable<K, V> merge(@NotNull MultiHashTable<K, V> tree) { // default merge strategy: append lists.
-            return this.merge(tree, ImmutableList::append);
-        }
-
-        @NotNull
-        public MultiHashTable<K, V> merge(@NotNull MultiHashTable<K, V> tree, @NotNull F2<ImmutableList<V>, ImmutableList<V>, ImmutableList<V>> merger) {
-            return new MultiHashTable<>(this.data.merge(tree.data, merger));
-        }
-
-        @NotNull
-        public ImmutableList<Pair<K, ImmutableList<V>>> entries() {
-            return this.data.entries();
-        }
-
-        // version: key is irrelevant
-        @NotNull
-        public <B> HashTable<K, B> toHashTable(@NotNull F<ImmutableList<V>, B> conversion) {
-            //return this.data.foldLeft((acc, p) -> acc.put(p.a, conversion.apply(p.b)), HashTable.empty(this.data.hasher));
-            return this.toHashTable((k, vs) -> conversion.apply(vs));
-        }
-
-        // version: key is used
-        @NotNull
-        public <B> HashTable<K, B> toHashTable(@NotNull F2<K, ImmutableList<V>, B> conversion) {
-            return this.data.foldLeft((acc, p) -> acc.put(p.a, conversion.apply(p.a, p.b)), HashTable.empty(this.data.hasher));
-        }
-
-        @NotNull
-        public final ImmutableList<ImmutableList<V>> values() {
-            return this.data.foldLeft((acc, p) -> acc.cons(p.b), ImmutableList.nil());
-        }
-
-        @NotNull
-        public final ImmutableList<V> gatherValues() {
-            return this.data.foldLeft((acc, p) -> acc.append(p.b), ImmutableList.nil());
-        }
-
-        @NotNull
-        public final <B> MultiHashTable<K, B> mapValues(@NotNull F<V, B> f) {
-            return new MultiHashTable<>(this.data.map(l -> l.map(f::apply)));
-        }
-    }
 
     private static final class EarlyErrorContextMonoid implements Monoid<EarlyErrorState> {
         @NotNull
