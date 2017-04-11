23958cb9418aeff9e14f1eb32381bf726873c94f
Kevin Gibbons <kevin@shapesecurity.com>
2017-04-10 18:21:30 -0700

    Do not include template delimiters in TemplateElement location

--- src/main/java/com/shapesecurity/shift/es2016/parser/ParserWithLocation.java
+++ src/main/java/com/shapesecurity/shift/es2016/parser/ParserWithLocation.java
@@ -1,10 +1,14 @@
 package com.shapesecurity.shift.es2016.parser;
 
 import com.shapesecurity.functional.data.HashTable;
+import com.shapesecurity.functional.data.ImmutableList;
 import com.shapesecurity.functional.data.Maybe;
+import com.shapesecurity.shift.es2016.ast.ExpressionTemplateElement;
 import com.shapesecurity.shift.es2016.ast.Module;
 import com.shapesecurity.shift.es2016.ast.Node;
 import com.shapesecurity.shift.es2016.ast.Script;
+import com.shapesecurity.shift.es2016.ast.TemplateElement;
+import com.shapesecurity.shift.es2016.ast.TemplateExpression;
 import org.jetbrains.annotations.NotNull;
 
 public class ParserWithLocation {
@@ -39,6 +43,18 @@ public class ParserWithLocation {
 				// Special case: the start/end of the whole-program node is the whole text including leading and trailing whitespace.
 				locations = locations.put(node, new SourceSpan(Maybe.empty(), new SourceLocation(0, 0, 0), new SourceLocation(this.startLine, this.startIndex - this.startLineStart, this.startIndex)));
 				return node;
+			} else if (node instanceof TemplateExpression) {
+				// Special case: adjust the locations of TemplateElement to not include surrounding backticks or braces
+				ImmutableList<ExpressionTemplateElement> elements = ((TemplateExpression) node).elements;
+				for (int i = 0; i < elements.length; i += 2) {
+					int endAdjustment = (i < elements.length - 1) ? 2 : 1; // discard '${' or '`' respectively
+
+					TemplateElement element = (TemplateElement) elements.index(i).fromJust();
+					SourceSpan oldLocation = locations.get(element).fromJust();
+					SourceLocation newStart = new SourceLocation(oldLocation.start.line, oldLocation.start.column + 1, oldLocation.start.offset + 1); // discard '}' or '`'
+					SourceLocation newEnd = new SourceLocation(oldLocation.end.line, oldLocation.end.column - endAdjustment, oldLocation.end.offset - endAdjustment);
+					locations = locations.put(element, new SourceSpan(Maybe.empty(), newStart, newEnd));
+				}
 			}
 			SourceLocation endLocation = this.getLastTokenEndLocation();
 			locations = locations.put(node, new SourceSpan(Maybe.empty(), startLocation, endLocation));
--- src/test/java/com/shapesecurity/shift/es2016/parser/miscellaneous/LocationTest.java
+++ src/test/java/com/shapesecurity/shift/es2016/parser/miscellaneous/LocationTest.java
@@ -5,10 +5,13 @@ import com.shapesecurity.shift.es2016.ast.BinaryExpression;
 import com.shapesecurity.shift.es2016.ast.Expression;
 import com.shapesecurity.shift.es2016.ast.ExpressionStatement;
 import com.shapesecurity.shift.es2016.ast.ExpressionSuper;
+import com.shapesecurity.shift.es2016.ast.ExpressionTemplateElement;
 import com.shapesecurity.shift.es2016.ast.Node;
 import com.shapesecurity.shift.es2016.ast.Script;
 import com.shapesecurity.shift.es2016.ast.Statement;
 import com.shapesecurity.shift.es2016.ast.StaticMemberExpression;
+import com.shapesecurity.shift.es2016.ast.TemplateElement;
+import com.shapesecurity.shift.es2016.ast.TemplateExpression;
 import com.shapesecurity.shift.es2016.parser.JsError;
 import com.shapesecurity.shift.es2016.parser.ParserWithLocation;
 import com.shapesecurity.shift.es2016.parser.SourceLocation;
@@ -63,4 +66,45 @@ public class LocationTest extends TestCase  {
 		checkText(object, "1.");
 	}
 
+	@Test
+	public void testSimpleTemplate() throws JsError {
+		init("`foo`;");
+		checkText(this.tree, this.source);
+
+		Statement statement = this.tree.statements.maybeHead().fromJust();
+		checkText(statement, "`foo`;");
+
+		Expression expression = ((ExpressionStatement) statement).expression;
+		checkText(expression, "`foo`");
+
+		ExpressionTemplateElement element = ((TemplateExpression) expression).elements.maybeHead().fromJust();
+		checkText(element, "foo");
+	}
+
+	@Test
+	public void testComplexTemplate() throws JsError {
+		init("`foo ${ 0 } bar ${ 1 } baz`;");
+		checkText(this.tree, this.source);
+
+		Statement statement = this.tree.statements.maybeHead().fromJust();
+		checkText(statement, "`foo ${ 0 } bar ${ 1 } baz`;");
+
+		Expression expression = ((ExpressionStatement) statement).expression;
+		checkText(expression, "`foo ${ 0 } bar ${ 1 } baz`");
+
+		ExpressionTemplateElement element = ((TemplateExpression) expression).elements.index(0).fromJust();
+		checkText(element, "foo ");
+
+		element = ((TemplateExpression) expression).elements.index(1).fromJust();
+		checkText(element, "0");
+
+		element = ((TemplateExpression) expression).elements.index(2).fromJust();
+		checkText(element, " bar ");
+
+		element = ((TemplateExpression) expression).elements.index(3).fromJust();
+		checkText(element, "1");
+
+		element = ((TemplateExpression) expression).elements.index(4).fromJust();
+		checkText(element, " baz");
+	}
 }
