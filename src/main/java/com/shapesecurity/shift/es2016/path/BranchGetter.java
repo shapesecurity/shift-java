a8d84342515d48adc7adf87c12b3585a7fa54632
Kevin Gibbons <kevin@shapesecurity.com>
2017-12-14 17:35:09 -0800

    Add .equals and .hashCode to Branch and BranchGetter

--- src/main/java/com/shapesecurity/shift/es2016/path/BranchGetter.java
+++ src/main/java/com/shapesecurity/shift/es2016/path/BranchGetter.java
@@ -4,6 +4,8 @@ import com.shapesecurity.functional.data.ImmutableList;
 import com.shapesecurity.functional.data.Maybe;
 import com.shapesecurity.shift.es2016.ast.Node;
 
+import java.util.Objects;
+
 public class BranchGetter {
     private ImmutableList<Branch> directions;
 
@@ -28,5 +30,18 @@ public class BranchGetter {
             directions = directions.maybeTail().fromJust();
         }
         return n;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BranchGetter that = (BranchGetter) o;
+        return Objects.equals(directions, that.directions);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(directions);
     }
 }
